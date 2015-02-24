2029e0834224dcf118d9d6e3e0c02043d6c4b2b6
Indrek Ots <ots.indrek@gmail.com>
2015-02-24 23:55:33 +0200

    validate game width and height upon creation
    Test, when creating a new game object, it has its width and height correctly set.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,8 +1,18 @@
 var game_obj = require('../app/game.js');
 
-describe("Game", function() {
+describe("Game, when created", function() {
 	it("should throw error when canvas width and height are not provided", function() {
 		expect(function() {new game_obj.Game({})})
 			.toThrow(new Error("Width and height are not provided!"));
+	});
+
+	it("should have width and height set when provided", function() {
+		var config = {
+			width: 800,
+			height: 600
+		};
+		var game = new game_obj.Game(config);
+		expect(game.config.width).toEqual(800);
+		expect(game.config.height).toEqual(600);
 	});
 });
