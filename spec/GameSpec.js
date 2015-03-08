5325ca358cdabc9654896e16a35e41ed18f7e8cf
Indrek Ots <ots.indrek@gmail.com>
2015-03-08 18:50:29 +0200

    add new config properties

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,32 +1,36 @@
 var game_obj = require('../src/js/Game.js');
 
 describe("Game, when created without config object", function() {
-    it("should throw error when canvas width and height are not provided", function() {
-        expect(function() {new game_obj.Game({})})
-            .toThrow(new Error("Width and height are not provided!"));
+    it("should throw error when no config object is provided", function() {
+        expect(function() {new game_obj.Game()})
+            .toThrow(new Error("No config provided!"));
     });
 });
 
 describe("Game, when created", function() {
     var game;
     var config = {
-        width: 800,
-        height: 600
+        canvasWidth: 800,
+        canvasHeight: 600,
+        cellWidth: 10,
+        cellHeight: 10
     };
 
     beforeEach(function() {
         game = new game_obj.Game(config);
     });
 
-    it("should have width and height set", function() {
-        expect(game.config.width).toEqual(800);
-        expect(game.config.height).toEqual(600);
+    it("should have required fields set", function() {
+        expect(game.config.canvasWidth).toEqual(800);
+        expect(game.config.canvasHeight).toEqual(600);
+        expect(game.config.boardWidth).toEqual(80);
+        expect(game.config.boardHeight).toEqual(60);
     });
 
     it("should initialize game board", function() {
         expect(game.board).not.toBeUndefined();
-        expect(game.board.length).toEqual(800);
-        expect(game.board[0].length).toEqual(600);
+        expect(game.board.length).toEqual(80);
+        expect(game.board[0].length).toEqual(60);
     });
 
     it("should have an empty board", function() {
@@ -45,8 +49,10 @@ describe("Game, when created", function() {
 describe("Game", function() {
     var game;
     var config = {
-        width: 800,
-        height: 600
+        canvasWidth: 800,
+        canvasHeight: 600,
+        cellWidth: 10,
+        cellHeight: 10
     };
 
     beforeEach(function() {
@@ -64,8 +70,8 @@ describe("Game", function() {
     });
 
     it("can not place a live cell over the right edge of the board", function() {
-        expect(function() {game.placeLiveCell(config.width + 5, 3)})
-            .toThrow(new game_obj.OutOfBoundsError(config.width + 5, 3));
+        expect(function() {game.placeLiveCell(85, 3)})
+            .toThrow(new game_obj.OutOfBoundsError(85, 3));
     });
 
     it("can not place a live cell over the top edge of the board", function() {
@@ -74,8 +80,8 @@ describe("Game", function() {
     });
 
     it("can not place a live cell over the bottom edge of the board", function() {
-        expect(function() {game.placeLiveCell(2, config.height + 3)})
-            .toThrow(new game_obj.OutOfBoundsError(2, config.height + 3));
+        expect(function() {game.placeLiveCell(2, 63)})
+            .toThrow(new game_obj.OutOfBoundsError(2, 63));
 
     });
 })
--- src/js/Game.js
+++ src/js/Game.js
@@ -1,24 +1,43 @@
 /*jslint plusplus: true */
 function Game(config) {
     "use strict";
-    if (typeof config.width === "undefined"  
-            || typeof config.height === "undefined") {
-        throw new Error("Width and height are not provided!");
+    function initSettings() {
+        config.boardWidth = config.canvasWidth / config.cellWidth;
+        config.boardHeight = config.canvasHeight / config.cellHeight;
     }
 
     function initBoard() {
         var arr = [], i, j;
-        for (i = 0; i < config.width; i++) {
+        for (i = 0; i < config.boardWidth; i++) {
             arr[i] = [];
-            for (j = 0; j < config.height; j++) {
+            for (j = 0; j < config.boardHeight; j++) {
                 arr[i][j] = 0;
             }
         }
         return arr;
     }
 
+    if (typeof config === "undefined") {
+        throw new Error("No config provided!");
+    }
+
+    initSettings();
     this.config = config;
     this.board = initBoard();
+}
+
+Game.prototype.placeLiveCell = function (x, y) {
+    "use strict";
+    var config = this.config;
+    function validate(x, y) {
+        if (x < 0 || x > config.boardWidth ||
+                y < 0 || y > config.boardHeight) {
+            throw new OutOfBoundsError(x, y);
+        }
+    }
+
+    validate(x, y);
+    this.board[x][y] = 1;
 }
 
 function OutOfBoundsError(x, y) {
@@ -28,20 +47,6 @@ function OutOfBoundsError(x, y) {
 }
 
 OutOfBoundsError.prototype = new Error();
-
-Game.prototype.placeLiveCell = function (x, y) {
-    "use strict";
-    var config = this.config;
-    function validate(x, y) {
-        if (x < 0 || x > config.width ||
-                y < 0 || y > config.height) {
-            throw new OutOfBoundsError(x, y);
-        }
-    }
-
-    validate(x, y);
-    this.board[x][y] = 1;
-}
 
 exports.OutOfBoundsError = OutOfBoundsError;
 exports.Game = Game;
